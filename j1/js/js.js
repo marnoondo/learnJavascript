4ffc6273605811f2c6bf60faf8d05db5535f1b4c
marnoondo <254062815@qq.com>
2017-05-24 21:11:20 +0800

    jqt

--- j1/js/js.js
+++ j1/js/js.js
@@ -1,1 +1,28 @@
-$('#one').css('background','#bbffaa');
+// $('#one').css('background','#bbffaa');
+// $('.mini').css('background','#bbffaa');
+// $('div').css('background','#bbffaa');
+// $('*').css('background','#bbffaa')
+// $('span,#two').css('background','#bbffaa');
+// $('body div').css('background','#bbffaa');
+// $('body>div').css('background','#bbffaa')
+// $('.one + div').css('background','#bbffaa');
+// $('.one ~ div').css('background','#bbffaa')
+// $('.one').next('div').css('background','#bbffaa');
+// $('.one').nextAll('div').css('background','#bbffaa')
+// $('div:first').css('background','#bbffaa');
+// $('div:last').css('background','#bbffaa')
+// $('div:not(.one)').css('background','#bbffaa')
+// $('div:even').css('background','#bbffaa')
+// $('div:odd').css('background','#bbffaa')
+// $('div:eq(3)').css('background','#bbffaa')
+// $('div:gt(3)').css('background','#bbffaa')
+// $('div:lt(3)').css('background','#bbffaa')
+// $(':header').css('background','#bbffaa')
+// $(':focus').css('background','#bbffaa')
+// $('div:contains(di)').css('background','#bbffaa')
+// $('div:empty').css('background','#bbffaa')
+// $('div:has(p)').css('background','#bbffaa')
+// $('div:parent').css('background','#bbffaa')
+// $('div:hidden').show(3000);
+
+
