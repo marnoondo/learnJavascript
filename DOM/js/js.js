db08e9d9701d7b398904523f11aa52d4f196a5a7
marnoondo <254062815@qq.com>
2017-05-25 10:09:22 +0800

    DOM

---
+++ DOM/js/js.js
@@ -0,0 +1,19 @@
+// var $li=$('ul li:eq(1)').css('background','red')
+// var $li_list=$li.text();
+// var $para=$('p');
+// var $p_txt=$para.attr('title')
+// alert($p_txt)
+// var $li_1=$('<li title="香蕉">香蕉</li>')
+// console.log($li_1)
+// var $li_2=$('<li title="雪梨">雪梨</li>');
+// $('ul').append($li_1)
+// $('ul').append($li_2)
+// var $li=$('ul li:eq(0)').attr({'title':'我换一下title','name':'写下名字'})
+// var $li=$('ul li:eq(0)').css({'background':'red','font-size':'30px'})
+// console.log($li)
+// var $p_html=$('p').html();
+// alert($p_html)
+// var $body=$('body').children();
+// console.log($body)
+var $p1=$('p').next();
+console.log($p1)
